daa7ef49ffc8a1a95f001c9cdb86448f52d0f8c5
Javier Julio <jjfutbol@gmail.com>
2022-06-07 18:03:58 -0400

    Remove react version code/files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders initial move count', () => {
-  render(<App />);
-  const label = screen.getByText(/0 moves/i);
-  expect(label).toBeInTheDocument();
-});
