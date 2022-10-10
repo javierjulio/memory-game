59be3d47769284ab40fd222012e65951c28f031a
Javier Julio <jjfutbol@gmail.com>
2022-10-09 22:17:10 -0400

    Use toHaveTextContent matcher

--- test/MoveCountLabel.test.js
+++ test/MoveCountLabel.test.js
@@ -5,21 +5,21 @@ describe('MoveCountLabel.svelte', () => {
   it('renders given move count', () => {
     const { container } = render(MoveCountLabel, { count: 0 });
     expect(container).toBeTruthy()
-    expect(container.innerHTML).toContain('0 moves')
+    expect(container).toHaveTextContent('0 moves')
     expect(container.innerHTML).toMatchSnapshot()
   })
 
   it('renders label in singular form with count of 1', () => {
     const { container } = render(MoveCountLabel, { count: 1 });
     expect(container).toBeTruthy()
-    expect(container.innerHTML).toContain('1 move')
+    expect(container).toHaveTextContent('1 move')
     expect(container.innerHTML).toMatchSnapshot()
   })
 
   it('renders label in plural form with count of 2', () => {
     const { container } = render(MoveCountLabel, { count: 2 });
     expect(container).toBeTruthy()
-    expect(container.innerHTML).toContain('2 moves')
+    expect(container).toHaveTextContent('2 moves')
     expect(container.innerHTML).toMatchSnapshot()
   })
 })
