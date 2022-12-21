8be66a4b3b132413f58e7794b2d2bed223f60f0f
Javier Julio <jjfutbol@gmail.com>
2022-12-20 22:54:55 -0500

    Create RecordsButton.test.js

---
+++ src/RecordsButton.test.js
@@ -0,0 +1,23 @@
+import { render, screen, fireEvent } from '@testing-library/svelte'
+import RecordsButton from './RecordsButton.svelte'
+
+describe("RecordsButton", () => {
+  it("includes button label", () => {
+    render(RecordsButton)
+    expect(screen.queryByRole("button", { name: "Records" })).toBeInTheDocument()
+  })
+
+  it("forwards the click event", async () => {
+    const { component } = render(RecordsButton)
+
+    let clickReceived = false
+    const off = component.$on('click', () => clickReceived = true)
+
+    const button = screen.queryByRole("button", { name: "Records" })
+    await fireEvent.click(button)
+
+    expect(clickReceived).toEqual(true)
+
+    off()
+  })
+})
