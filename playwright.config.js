c4719fc5f74b6841068cddca9e5850fa432ef5e1
Javier Julio <jjfutbol@gmail.com>
2023-01-03 16:49:19 -0500

    Update config to forbid test.only and use retries

--- playwright.config.js
+++ playwright.config.js
@@ -2,12 +2,14 @@
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
+  forbidOnly: !!process.env.CI, // Fail CI build if accidentally left test.only in code
+  testDir: "tests",
+  retries: process.env.CI ? 2 : 0,
   webServer: {
     command: "npm run build && npm run preview",
     port: 4173
   },
   workers: process.env.CI ? 2 : undefined,
-  testDir: "tests"
 }
 
 export default config
